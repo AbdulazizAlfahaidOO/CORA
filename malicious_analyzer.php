f4c765c9bdbee36760d684c341329e54d5747428
AbdulazizAlfahaidOO <65142904+AbdulazizAlfahaidOO@users.noreply.github.com>
2022-12-05 04:38:32 +0300

    Add files via upload

---
+++ malicious_analyzer.php
@@ -0,0 +1,27 @@
+<?php
+include 'db_connect.php';
+
+?>
+<div class="col-lg-12">
+      <?php if(isset($_SESSION['login_id'])): ?>
+	
+	</div>
+    <?php endif; ?>
+	<div class="row">
+		<div class="col-md-7">
+			<div class="card card-outline card-info">
+				<div class="card-header">
+					<div class="card-tools">
+						<small class="text-muted">
+						</small>
+					</div>
+				</div>
+				<div class="card-body">
+                <dd></dd>
+						</dl>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
