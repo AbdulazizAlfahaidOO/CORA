44feaf2babcb1d9ecb54c797a722d722d02842f8
AAShah <megananospeed@gmail.com>
2022-12-15 17:30:01 +0300

    New UI for Login Page

--- login.php
+++ login.php
@@ -21,17 +21,17 @@ header("location:ind.php?page=home");
 </head>
 <style>
 	body{
-		
 		width: 100%;
 	    height: calc(100%);
 	    position: fixed;
 	    top:0;
-	    left: 0
+	    left: 0;
 	}
 	main#main{
 		width:100%;
 		height: calc(100%);
 		display: flex;
+		background-color: #052e39;
 	}
 	#content {
         position: relative;
@@ -43,33 +43,64 @@ header("location:ind.php?page=home");
 		width: 5%;
         height: auto;
     }
+	.ribbon {
+		height: 80px;
+		width: 70px;
+	}
+	.card {
+		box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+	}
+
+	.login__field {
+	padding: 20px 0px;	
+	position: relative;	
+	}
+	.login__icon {
+		position: absolute;
+		top: 35px;
+		color: #000000;
+	}
+
+	.login__input {
+		border: none;
+		border-bottom: 2px solid #D1D1D4;
+		background: none;
+		padding: 10px;
+		padding-left: 24px;
+		font-weight: 700;
+		width: 100%;
+		transition: .2s;
+	}
+
+	.login__input:active,
+	.login__input:focus,
+	.login__input:hover {
+		outline: none;
+		border-bottom-color: #6A679E;
+	}
 </style>
 
 <body class="bg-white" >
 
-<div id="content">
-	<a href="index.html">
-    <img src="images/CORA-LOGO.png" class="ribbon" alt="" />
-	</a>
-</div>
-
   <main id="main" >
   	
   		<div class="align-self-center w-100" >
-		<h4 class="text-dark text-center"><b>CORA Log In</b></h4>
-  		<div id="login-center" class="bg-white row justify-content-center">
-  			<div class="card col-md-4">
+  		<div id="login-center" class="row justify-content-center">
+  			<div class="card col-lg-2 col-md-4 col-sm-10">
   				<div class="card-body">
+				  <center><img src="images/CORA-LOGO.png" class="ribbon" alt="" /></center> <br>
+				  <center><h1>Log In</h1></center>
   					<form id="login-form" >
-  						<div class="form-group">
-  							<label for="email" class="control-label text-dark">Email</label>
-  							<input type="text" id="email" name="email" class="form-control form-control-sm">
-  						</div>
-  						<div class="form-group">
-  							<label for="password" class="control-label text-dark">Password</label>
-  							<input type="password" id="password" name="password" class="form-control form-control-sm">
-  						</div>
-  						<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+						<div class="login__field">
+							<i class="login__icon fas fa-user"></i>
+							<input type="text" class="login__input" placeholder="Email">
+						</div>
+						<div class="login__field">
+							<i class="login__icon fas fa-lock"></i>
+							<input type="password" class="login__input" placeholder="Password">
+						</div>
+  						<left><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></left><br>
+						<p><a href="/ajax1.php">Dont have an Account?</a></p>
   					</form>
   				</div>
   			</div>
