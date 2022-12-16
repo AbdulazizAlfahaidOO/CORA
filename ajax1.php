d06390b705f19b6cc8eb4ce2b0f14e181c5f829f
AAShah <megananospeed@gmail.com>
2022-12-17 01:22:54 +0300

    New Yara Rule
    NEW YARA RULE

--- ajax1.php
+++ ajax1.php
@@ -57,82 +57,82 @@
   <script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
  <!-- summernote -->
   <link rel="stylesheet" href="assets/plugins/summernote/summernote-bs4.min.css">
+  <link rel=""
   </head>
-<h1>Register </h1>
-<form action="/ajax1.php" method="post" enctype="multipart/form-data" >
+  <style>
+.regsiter__field {
+	padding: 20px 0px;	
+	position: relative;	
+}
+.regsiter__input {
+	border: none;
+	border-bottom: 2px solid #000000;
+	background: none;
+	padding: 10px;
+	padding-left: 14px;
+	font-weight: 700;
+	width: 100%;
+	transition: .2s;
+}
+.regsiter__input:active,
+.regsiter__input:focus,
+.regsiter__input:hover {
+	outline: none;
+	border-bottom-color: #000000;
+}
+  </style>
+  <body class="bg-white" >
+			<form action="/ajax1.php" method="post" enctype="multipart/form-data" >
+			<h1>Register </h1>
 				<input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
-				<div class="row">
-					<div class="col-md-6 border-right">
-						<b class="text-muted">Personal Information</b>
-						<div class="form-group">
-							<label for="" class="control-label">First Name</label>
-							<input type="text" name="firstname" class="form-control form-control-sm" required value="<?php echo isset($firstname) ? $firstname : '' ?>">
-						</div>
-						<div class="form-group">
-							<label for="" class="control-label">Middle Name</label>
-							<input type="text" name="middlename" class="form-control form-control-sm"  value="<?php echo isset($middlename) ? $middlename : '' ?>">
-						</div>
-						<div class="form-group">
-							<label for="" class="control-label">Last Name</label>
-							<input type="text" name="lastname" class="form-control form-control-sm" required value="<?php echo isset($lastname) ? $lastname : '' ?>">
-						</div>
-						<div class="form-group">
-							<label for="" class="control-label">Contact No.</label>
-							<input type="text" name="contact" class="form-control form-control-sm" required value="<?php echo isset($contact) ? $contact : '' ?>">
-						</div>
-						<div class="form-group">
-							<label class="control-label">Address</label>
-							<textarea name="address" id="" cols="30" rows="4" class="form-control" required><?php echo isset($address) ? $address : '' ?></textarea>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-						<b class="text-muted">System Credentials</b>
-                        <br>
-							<label class="control-label">Email</label>
-							<input type="email" class="form-control form-control-sm" name="email" required value="<?php echo isset($email) ? $email : '' ?>">
-							<small id="#msg"></small>
-						</div>
-						<div class="form-group">
-							<label class="control-label">Password</label>
-							<input type="password" class="form-control form-control-sm" name="password" <?php echo isset($id) ? "":'required' ?>>
-							<small><i><?php echo isset($id) ? "Leave this blank if you dont want to change you password":'' ?></i></small>
-						</div>
-						<div class="form-group">
-							<label class="label control-label">Confirm Password</label>
-							<input type="password" class="form-control form-control-sm" name="cpass" <?php echo isset($id) ? 'required' : '' ?>>
-							<small id="pass_match" data-status=''></small>
-                            <a href="login.php" class="ca">Already have an account?</a>
-						</div>
+				<div class="row justify-content-center">
+					<div class="col-md-6 border-center" id="personalInfo-box">
+							<b class="text-muted">Personal Information</b>
+							<div class="regsiter__field form-group">
+								<input type="text" name="firstname" id="firstname" class="regsiter__input" placeholder="First Name" required value="<?php echo isset($firstname) ? $firstname : '' ?>">
+							</div>
+							<div class="regsiter__field form-group">
+								<input type="text" name="middlename" id="middlename" class="regsiter__input" placeholder="Middle Name" required value="<?php echo isset($firstname) ? $firstname : '' ?>">
+							</div>
+							<div class="regsiter__field form-group">
+								<input type="text" name="lastname" id="lastname" class="regsiter__input" placeholder="Last Name" required value="<?php echo isset($firstname) ? $firstname : '' ?>">
+							</div>
+							<div class="regsiter__field form-group">
+								<input type="text" name="contact" id="contact" class="regsiter__input" placeholder="Contact No." required value="<?php echo isset($firstname) ? $firstname : '' ?>">
+							</div>
+							<div class="regsiter__field form-group">
+								<textarea name="address" id="address" cols="30" rows="4" class="regsiter__input" placeholder="Address" required><?php echo isset($address) ? $address : '' ?></textarea>
+							</div>
+							<button class="btn btn-primary mr-2" onclick="showPasswordBox()">Next</button>
+							<button class="btn btn-secondary" type="button" onclick="location.href = 'index.html'">Cancel</button>
+					</div>					
+					<div class="col-md-6" id="password-box">
+							<b class="text-muted">System Credentials</b>
+							<br>
+							<div class="regsiter__field form-group">
+								<input type="email" class="regsiter__input"  name="email" placeholder="Email" required value="<?php echo isset($email) ? $email : '' ?>">
+								<small id="#msg"></small>
+							</div>
+							<div class="regsiter__field form-group">
+								<input type="password" class="regsiter__input" name="password" placeholder="Password" <?php echo isset($id) ? "":'required' ?>>
+							</div>
+							<div class="regsiter__field form-group">
+								<input type="password" class="regsiter__input" name="cpass" placeholder="Confirm Password" <?php echo isset($id) ? 'required' : '' ?>>
+								<small id="pass_match" data-status=''></small>
+							</div>
 							<div class="custom-file">
-		                    </div>
-						</div>
-						<div class="form-group d-flex justify-content-center">
-							<img src="<?php echo isset($avatar) ? 'assets/uploads/'.$avatar :'' ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
-						</div>
-						<?php if(false): ?>
-						<div class="form-group">
-							<label for="" class="control-label">User Role</label>
-							<select name="type" id="type" class="custom-select custom-select-sm">
-								<option value="2" <?php echo isset($type) && $type == 2 ? 'selected' : '' ?>>User</option>
-								<option value="1" <?php echo isset($type) && $type == 1 ? 'selected' : '' ?>>Admin</option>
-							</select>
-						</div>
-						<?php else: ?>
-							<input type="hidden" name="type" value="3">
-						<?php endif; ?>
-						
+							</div>
+							<button class="btn btn-primary mr-2" name="submit" type="submit"onclick="location = 'login.php'">Save</button>
 					</div>
+				</div>
+
 				</div>
 				<hr>
-				<div class="col-lg-12 text-right justify-content-center d-flex">
-					<button class="btn btn-primary mr-2" name="submit" type="submit"onclick="location = 'login.php'">Save</button>
-					<button class="btn btn-secondary" type="button" onclick="location.href = 'index.html'">Cancel</button>
-				</div>
 			</form>
 		</div>
 	</div>
 </div>
+</body>
 
             
             <?php
@@ -243,7 +243,6 @@ if(isset($_POST['submit'])){
 				return false;
 			}
 		}
-        
 		$.ajax({
 			url:'ajax.php?action=save_user',
 			data: new FormData($(this)[0]),
@@ -266,4 +265,30 @@ if(isset($_POST['submit'])){
 			}
 		})
 	})
+	document.getElementById('password-box').style.display ='none';
+        function showPasswordBox(){
+            var personalInfo = document.getElementById('firstname, middlename, lastname, contact, address').value;
+            document.getElementById('personalInfo-box').style.display ='none';
+            document.getElementById('password-box').style.display ='block';
+        }
+        function onEyeClick(){
+            let ele = document.getElementById('eye-icon');
+            let password = document.getElementById('password');
+           
+           if(password.value.length > 0){
+            
+            document.getElementById('password').value = password.value;
+            password.focus();
+                //document.getElementById('password-label').classList.add('password-fixed');
+                if(ele.className == 'fa fa-eye-slash'){
+                    document.getElementById('eye-icon').classList.remove('fa-eye-slash');
+                    document.getElementById('eye-icon').classList.add('fa-eye');
+                    password.setAttribute('type','text');
+                }else{
+                    document.getElementById('eye-icon').classList.remove('fa-eye');
+                    document.getElementById('eye-icon').classList.add('fa-eye-slash');
+                    password.setAttribute('type','password');
+                }
+           }    
+        }
 </script>
